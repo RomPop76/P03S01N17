26d35eccc378a676aacecdc3c7585cc2d524adea
RomPop76 <r.popielas@gmail.com>
2023-04-11 20:26:50 +0200

    Commit 2

---
+++ main.js
@@ -0,0 +1,29 @@
+// Zadanie 1
+1.1
+let myVariable = 10;
+
+1.2
+let myName = "Benedykt";
+
+
+// Zadanie 2
+2.1 
+// Typ Number
+
+2.2
+// Typ string
+
+
+// Zadanie 3
+3.1
+myVariable = 11;
+
+3.2
+myName = "Mieszko";
+
+// Zadanie 4
+4.1
+const sum = myVariable + 5;
+
+4.2 
+const greeting = myName + ", witaj w świecie Javascript!";
